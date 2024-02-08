85aa5ac0511b107be95ad03ec0c2b53e91291ef2
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-02-08 18:42:27 +0200

    :)

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.deepPurple;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color grey = Colors.grey;
+}
